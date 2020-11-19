11dde7188322344635472064933ab1079ef99366
Suyog Subedi <subedisuyog555@gmail.com>
2020-11-19 18:54:57 +0545

    Added image from the local source|I am rich app completed

--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,8 @@ void main() {
         ),
         body: Center(
           child: (Image(
-            image: NetworkImage('https://image.shutterstock.com/image-photo/white-transparent-leaf-on-mirror-260nw-1029171697.jpg')
+            image:
+              AssetImage('images/diamond.jpg')
           )),
         ),
 
