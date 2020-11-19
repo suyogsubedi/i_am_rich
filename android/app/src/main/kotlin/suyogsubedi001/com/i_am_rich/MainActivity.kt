80cb926fd4dd23d1e0436aafd3168ac9fafac2ed
Suyog Subedi <subedisuyog555@gmail.com>
2020-11-19 17:46:39 +0545

    Scaffold||Layout Basics

---
+++ android/app/src/main/kotlin/suyogsubedi001/com/i_am_rich/MainActivity.kt
@@ -0,0 +1,6 @@
+package suyogsubedi001.com.i_am_rich
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+//Main function is the starting point for our flutter apps
+void main() {
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.blueGrey,
+
+        appBar: AppBar(
+          title: Text('I Am Rich'),
+          backgroundColor:Colors.blueGrey[900],
+        ),
+        body: Center(
+          child: (Image(
+            image: NetworkImage('https://image.shutterstock.com/image-photo/white-transparent-leaf-on-mirror-260nw-1029171697.jpg')
+          )),
+        ),
+
+      ),
+    ),
+  );
+}
